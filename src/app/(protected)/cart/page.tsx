b7727132f25bbd94660523b55e58edb6eea8c95e
shivamKcode <shivambaniya246@gmail.com>
2024-04-19 19:44:29 -0400

    ____

--- src/app/(protected)/cart/page.tsx
+++ src/app/(protected)/cart/page.tsx
@@ -1,30 +1,33 @@
 import CartItems from "@/components/block/page/cart/cartItems";
 import { getCartItems } from "@/lib/directus/methods";
 import { cookies } from "next/headers";
+import Link from "next/link";
 import React from "react";
 import { IoMdClose } from "react-icons/io";
 
 type Props = {};
-export default async function Page({ }: Props) {
-    const cartData = await getCartItems();
+export default async function Page({}: Props) {
+  const cartData = await getCartItems();
 
-    return (
-        <div className="m-auto grid grid-cols-1">
-            <div className="col-span-1 py-4 border-b border-slate-200 shadow-sm">
-                <div className="flex justify-between items-center container">
-                    <p className="font-semibold">My Cart</p>
-                    <IoMdClose className="w-4 h-4" />
-                </div>
-            </div>
-            <div className="container">
-                <div className="col-span-1 mt-2 px-2 py-3 rounded-lg bg-blue-200 text-blue-600">
-                    Store is open
-                </div>
-                <div className="col-span-1">
-                    <CartItems cartData={cartData} />
-                </div>
-            </div>
-            {/* <Button onClick={getCart}>Click me</Button> */}
+  return (
+    <div className="m-auto grid grid-cols-1">
+      <div className="col-span-1 py-4 border-b border-slate-200 shadow-sm">
+        <div className="flex justify-between items-center container">
+          <p className="font-semibold">My Cart</p>
+          <Link href={"/products"}>
+            <IoMdClose className="w-4 h-4" />
+          </Link>
         </div>
-    );
+      </div>
+      <div className="container">
+        <div className="col-span-1 mt-2 px-2 py-3 rounded-lg bg-blue-200 text-blue-600">
+          Store is open
+        </div>
+        <div className="col-span-1">
+          <CartItems cartData={cartData} />
+        </div>
+      </div>
+      {/* <Button onClick={getCart}>Click me</Button> */}
+    </div>
+  );
 }
