6256c847b767016b69375f514014d4d107ed0a7d
lokeshjha <lokesh1jha@gmai.com>
2024-04-21 01:45:49 +0530

    product + cart fixed

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,126 +1,118 @@
 import { responseHelper } from '@/lib/helpers';
-import { directus } from '@/lib/utils';
-import { readItems, updateItem, deleteItem, createItem} from '@directus/sdk';
 import { isAuthenticatedAndUserData } from '@/lib/auth';
-import { cookies, headers } from 'next/headers';
+import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    const headers = req.headers.values()
-    const authData = await isAuthenticatedAndUserData()
-    // if(!authData.isAuthenticated) {
+    const authData = await isAuthenticatedAndUserData();
+     // if(!authData.isAuthenticated) {
     //     const baseUrl = new URL(req.url).origin
     //     return NextResponse.redirect(new URL("/auth/login", baseUrl));
     // }
     // const user_id = authData.user?.id
-    let user_id = 5
-    const result = await directus.request(
-      // @ts-expect-error
-      readItems('cart', {
-        fields: ['quantity', 'id', 'quantity_id',  'quantity_id.*', 'product_id.*'],
-        filter: {
-          user_id: {
-            _eq: user_id,
-          }
-        }
-      })
-    );
+    let user_id = 5; // Sample user_id, replace it with actual user_id logic
+    
+    const cartItems:any = await prisma.$queryRaw`
+      SELECT * FROM cart
+      join products on cart.product_id = products.id
+      join quantity on cart.quantity_id = quantity.id
+      WHERE user_id = ${user_id}
+    `;
 
     return responseHelper(
       {
         message: "Cart items fetched successfully",
         statusCode: 200,
-        data: result,
+        data: cartItems,
       },
       200,
     );
 
   } catch (error) {
-    console.log(error)
-    return responseHelper({ message: "Internal Server Error", statusCode: 200, data: {} }, 200);
+    console.log(error);
+    return responseHelper({ message: "Internal Server Error", statusCode: 500, data: {} }, 500);
   }
 }
 
 export async function POST(req: Request) {
   try {
     const { productId, quantityId, quantity } = await req.json();
-    const authData = await isAuthenticatedAndUserData()
-    const userId = authData?.user?.id
+    const authData = await isAuthenticatedAndUserData();
+    const userId = authData?.user?.id;
   
-  if (quantity === undefined) {
-    return responseHelper(
-      {
-        message: "Please provide Product id and Quantity id",
-        statusCode: 400,
-        data: {},
-      },
-      200,
-    );
-  }
-  const result = await directus.request(
-    //@ts-ignore
-    readItems("cart", {
-      filter: {
-        user_id: {
-          _eq: userId,
+    if (quantity === undefined) {
+      return responseHelper(
+        {
+          message: "Please provide Product id and Quantity id",
+          statusCode: 400,
+          data: {},
         },
-        product_id: {
-            _eq: productId
-        },
-        quantity_id: {
-            _eq: quantityId
-        }
-      },
-    }),
-  );
-  
-  let cartId:number = result[0] ? result[0].id : null;
-
-  let apimessage = ''
-  if (quantity === 0 && cartId) {
-    // If quantity is 0, remove the item from the cart
-    await directus.request(deleteItem("cart", cartId));
-    apimessage = 'Product count updated'
-  } 
-  else 
-  if(cartId && quantity){
-    // Update quantity for the item in the cart
-    await directus.request(
-      updateItem("cart", cartId, { quantity: quantity }),
-    );
-    apimessage = 'Product count updated'
-  }
-  else 
-  if(productId && userId){
-    await directus.request(createItem("cart", {
+        200,
+      );
+    }
+    
+    let cartItem = await prisma.cart.findFirst({
+      where: {
         user_id: userId,
         product_id: productId,
         quantity_id: quantityId,
-        quantity: quantity || 1,
-        created_at: new Date().toISOString(),
-        updated_at: new Date().toISOString()
-    }))
-    apimessage = 'New product added to cart'
+      },
+    });
 
-  }
-  if(apimessage = '') {
-    return responseHelper(
+    let apimessage = '';
+    if (quantity === 0 && cartItem) {
+      // If quantity is 0, remove the item from the cart
+      await prisma.cart.delete({
+        where: {
+          id: cartItem.id,
+        },
+      });
+      apimessage = 'Product count updated';
+    } else if (cartItem && quantity) {
+      // Update quantity for the item in the cart
+      await prisma.cart.update({
+        where: {
+          id: cartItem.id,
+        },
+        data: {
+          quantity: quantity,
+        },
+      });
+      apimessage = 'Product count updated';
+    } else if (productId && userId) {
+      // Add a new product to the cart
+      await prisma.cart.create({
+        data: {
+          user_id: userId,
+          product_id: productId,
+          quantity_id: quantityId,
+          quantity: quantity || 1,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      });
+      apimessage = 'New product added to cart';
+    }
+
+    if (apimessage === '') {
+      return responseHelper(
         { message: "Failed to update cart", statusCode: 400, data: {} },
         200,
       );
-  }
+    }
 
-  return responseHelper(
-    { message: apimessage, statusCode: 200, data: {} },
-    200,
-  );
-  } catch (error) {
-    console.log(error)
     return responseHelper(
-        { message: "CART::POST Internal Server Error", statusCode: 500, data: {} },
-        500,
-      );
+      { message: apimessage, statusCode: 200, data: {} },
+      200,
+    );
+  } catch (error) {
+    console.log(error);
+    return responseHelper(
+      { message: "CART::POST Internal Server Error", statusCode: 500, data: {} },
+      500,
+    );
   }
 }
--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,15 +1,13 @@
 import { responseHelper } from "@/lib/helpers";
-import { directus } from "@/lib/utils";
-import {  readItems } from "@directus/sdk";
-import { Product } from "@/types/client/types";
-
+import prisma from "@/lib/prisma/client";
+import { Prisma } from "@prisma/client";
 
 export async function GET(req: Request) {
   try {
     const url = new URL(req.url);
     const categoryId = url.searchParams.get("categoryId");
 
-    if(!categoryId){
+    if (!categoryId) {
       return responseHelper(
         { message: "Category Id is required", statusCode: 400, data: {} },
         400,
@@ -17,18 +15,39 @@ export async function GET(req: Request) {
     }
 
     const category_id = Number(categoryId);
-    // @ts-expect-error
-    const products: Product[] = await directus.request(
-      // @ts-expect-error
-      readItems("products", {
-        fields: ["*", "quantity_id.*"],
-        filter: {
-          category_id: {
-            _eq: category_id,
-          },
-        },
-      }),
-    );
+    const products: any = await prisma.$queryRaw`
+    SELECT * FROM products
+    WHERE category_id = ${Number(category_id)}
+  `;
+
+    // Extract product IDs from the fetched products
+    const productIds = products.map((product: any) => Number(product.id));
+
+    const quantityOfProducts: any = await prisma.$queryRaw`
+    SELECT * FROM quantity
+    WHERE product_id IN (${Prisma.join(
+      productIds
+    )})`;
+
+
+
+    // Group quantities by product ID
+    const quantitiesByProductId: Record<number, any[]> = {};
+    quantityOfProducts.forEach((quantity: any) => {
+      if (!quantitiesByProductId[quantity.product_id]) {
+        quantitiesByProductId[quantity.product_id] = [];
+      }
+      quantitiesByProductId[quantity.product_id].push(quantity);
+    });
+
+    // Map quantities to their respective products
+    products.forEach((product: any) => {
+      // Find quantities associated with the current product
+      const productQuantities = quantitiesByProductId[product.id] || [];
+
+      // Add quantities to the product
+      product.quantities = productQuantities;
+    });
 
     return responseHelper(
       {
