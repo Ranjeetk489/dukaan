b34f4caf27d81c5f19b5179dcf5d4f52765b1a3a
Ranjeet Kumar <ranjeet.kumar@leverageedu.com>
2024-04-21 00:47:35 +0530

    fix

--- src/app/(protected)/cart/page.tsx
+++ src/app/(protected)/cart/page.tsx
@@ -1,6 +1,7 @@
 import OrderCart from "@/components/block/page/cart/cart";
 import CartItems from "@/components/block/page/cart/cartItems";
 import { getCartItems } from "@/lib/directus/methods";
+import prisma from "@/lib/prisma/client";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import React from "react";
@@ -8,8 +9,11 @@ import { IoMdClose } from "react-icons/io";
 
 type Props = {};
 export default async function Page({}: Props) {
-  const cartData = await getCartItems();
-  console.log(cartData, "cartData")
+//   const cartData = await getCartItems();
+//   console.log(cartData, "cartData")
+    const data = await prisma.$queryRaw`SELECT * FROM cart`
+    console.log(data, "data")
+
 
     return (
         <div className="m-auto grid grid-cols-1">
@@ -23,9 +27,9 @@ export default async function Page({}: Props) {
                 <div className="col-span-1 mt-2 px-2 py-3 rounded-lg bg-blue-200 text-blue-600">
                     Store is open
                 </div>
-                <div className="col-span-1">
+                {/* <div className="col-span-1">
                     <OrderCart cartData={cartData}/>
-                </div>
+                </div> */}
             </div>
         </div>
   );
